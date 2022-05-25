9e980087f06e1ffd49d78a489206f9c3d32915b5
Idric Evarne <idric.evarne@gmail.com>
2022-05-25 18:29:39 +0200

    Activation des du code après modifications de Anthoine

---
+++ K-30_1.py
@@ -0,0 +1,44 @@
+def somme(liste):
+    _somme = 0
+    for i in liste:
+        _somme = _somme + i
+    return _somme
+
+def moyenne(liste):
+    return somme(liste)/len(liste)
+
+#rpi serial connections
+#Python app to run a K-30 Sensor
+import serial
+import time
+import sys
+
+ser = serial.Serial("/dev/ttyUSB0",baudrate =9600,timeout = .5)
+ser.flushInput()
+time.sleep(1)
+
+limitMesure = int(sys.argv[1])
+for i in range(1, 10):
+    ser.flushInput()
+    ser.write("\xFE\x44\x00\x08\x02\x9F\x25")
+    time.sleep(.5)
+    resp = ser.read(7)
+    high = ord(resp[3])
+    low = ord(resp[4])
+    co2 = (high*256) + low
+    time.sleep(.5)
+
+
+listCo2 = []
+for i in range(1, limitMesure):
+    ser.flushInput()
+    ser.write("\xFE\x44\x00\x08\x02\x9F\x25")
+    time.sleep(.5)
+    resp = ser.read(7)
+    high = ord(resp[3])
+    low = ord(resp[4])
+    co2 = (high*256) + low
+    listCo2.append(co2 * 10)
+    time.sleep(.5)
+
+print (moyenne(listCo2))
--- note.md
+++ note.md
@@ -1,1 +1,26 @@
 //! -------------------------------------------------
+const SerialPort = require('serialport');
+
+SerialPort.list((err, ports) => {
+console.log(ports)  
+})
+
+var myPort = new SerialPort('COM3', {
+baudRate:9600
+})
+
+myPort.on('open', onOpen);
+myPort.on('data', onData);
+myPort.write('M01');
+
+function onOpen(){
+console.log("Open connection");
+}
+
+function onData(data){
+console.log("on Data " + data);
+}
+
+while(true){
+console.log(myPort.read())
+}
